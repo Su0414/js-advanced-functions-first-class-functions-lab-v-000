8a0239d879903b2051f63d9ce506453ebc71b989
Suchitra Naik <susanaik@gmail.com>
2018-07-12 13:35:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+const drivers = {};
+
+function returnFirstTwoDrivers(drivers){
+  const twoDrivers = function (drivers){
+    return {drivers[0], drivers[1]};
+  }
+}
