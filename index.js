606be9b43d8322ae3a127c95dae11cf1581f8134
Suchitra Naik <susanaik@gmail.com>
2018-07-12 13:43:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function returnFirstTwoDrivers(drivers){
   const firstTwoDrivers = function (drivers){
     return {drivers[0], drivers[1]};
   };
-  firstTwoDrivers
+
 }
 
 function returnLastTwoDrivers(drivers){
